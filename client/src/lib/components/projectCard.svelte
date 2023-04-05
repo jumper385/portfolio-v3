9592ab7bc1157e69281fadbed9685c375e2af754
Henry Chen <henrychen385@gmail.com>
2023-04-06 07:20:00 +0800

    improved image sizing for project cards

--- client/src/lib/components/projectCard.svelte
+++ client/src/lib/components/projectCard.svelte
@@ -5,16 +5,20 @@
 <div class="project-card">
     <img src={imgUrl} alt={description} style="width:100%" />
 
-    <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
+    <div class="card-details">
+        <h3 class="title">{title}</h3>
+        <p class="Description">{description}</p>
     </div>
 </div>
 
-<style>
+<style lang="scss">
     .project-card {
         display: flex;
         flex-direction: column;
         align-items: center;
+        img {
+            width: 100%;
+            resize: both;
+        }
     }
 </style>
