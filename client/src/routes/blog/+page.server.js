86cf7f38a6fb6090c4a79c9fd5ef3a226f734dd7
Henry Chen <henrychen385@gmail.com>
2023-04-07 23:15:49 +0800

    loading blog data by filtering keys

--- client/src/routes/blog/+page.server.js
+++ client/src/routes/blog/+page.server.js
@@ -4,6 +4,15 @@ const pb = new Pocketbase(import.meta.env.VITE_DB_PATH);
 
 export const load = async () => {
   let out = await pb.collection('blog').getFullList(200);
-  let blogs = structuredClone(out);
+  let blogs =
+      out.map(e => ({
+                id: e.id,
+                author: e.author,
+                title: e.title,
+                displayImg: pb.getFileUrl(e, e.displayImg, {thumb: '0x1000'}),
+                likes: e.likes,
+                views: e.views
+              }))
+  console.log(blogs)
   return {blogs};
 }
