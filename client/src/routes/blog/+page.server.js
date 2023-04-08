7aa1e4e1a10d619594763e0c9392c687f463a392
Henry Chen <henrychen385@gmail.com>
2023-04-08 12:26:23 +0800

    only visible blogs will display now

--- client/src/routes/blog/+page.server.js
+++ client/src/routes/blog/+page.server.js
@@ -3,16 +3,24 @@ import Pocketbase from 'pocketbase';
 const pb = new Pocketbase(import.meta.env.VITE_DB_PATH);
 
 export const load = async () => {
-  let out = await pb.collection('blog').getFullList(200);
-  let blogs =
-      out.map(e => ({
-                id: e.id,
-                author: e.author,
-                title: e.title,
-                displayImg: pb.getFileUrl(e, e.displayImg, {thumb: '0x1000'}),
-                likes: e.likes,
-                views: e.views,
-                category: e.category,
-              }))
+  let {items: out} =
+      await pb.collection('blog').getList(1, 50, {filter: 'visible = true'});
+  console.log(out?.map(e => e.id))
+  let blogs;
+  if (out.length > 0) {
+    blogs = out?.map(
+        e => ({
+          id: e.id,
+          author: e.author,
+          title: e.title,
+          displayImg: pb.getFileUrl(e, e.displayImg, {thumb: '0x1000'}),
+          likes: e.likes,
+          views: e.views,
+          category: e.category,
+        }))
+  } else {
+    blogs = []
+  }
+
   return {blogs};
 }
