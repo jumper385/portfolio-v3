5d0db77cc78eba9725681135848c6a8ce69e2b7a
Henry Chen <henrychen385@gmail.com>
2023-05-07 16:05:16 +0800

    changed redirect...

--- client/src/routes/projects/+page.server.js
+++ client/src/routes/projects/+page.server.js
@@ -21,7 +21,7 @@ export const actions = {
 
     try {
       session = await stripe.checkout.sessions.create({
-        success_url: 'http://10.1.1.27:5173/projects',
+        success_url: 'http://digism.xyz/projects',
         line_items: [{quantity: 1, price: price_code}],
         mode: 'payment',
         automatic_tax: {enabled: true}
