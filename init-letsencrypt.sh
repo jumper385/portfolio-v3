456f23636ab66bf1947fdceec7ce5b5aa665e31f
Henry Chen <henrychen385@gmail.com>
2023-05-07 21:12:52 +0800

    changed letencrypte

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(digism.xyz www.digism.xyz)
+domains=(mail.digism.xyz digism.xyz www.digism.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="test@digism.xyz" # Adding a valid address is strongly recommended
