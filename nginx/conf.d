34516c6e3c682b68d596d2acb10717a527ecff06
Henry Chen <henrychen385@gmail.com>
2023-05-07 17:02:37 +0800

    added 443 configs

--- nginx/conf.d
+++ nginx/conf.d
@@ -15,6 +15,12 @@ server {
     listen 443 ssl;
     server_name digism.xyz;
 
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
+    ssl_certificate /etc/letsencrypt/live/digism.xyz/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/digism.xyz/privkey.pem;
+
     location / {
         proxy_pass http://digism.xyz; #for demo purposes
     }
