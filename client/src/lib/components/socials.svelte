0eb78031c993b1c691c7fe1d6cfcb7511bf6e544
Henry Chen <henrychen385@gmail.com>
2023-05-20 14:44:25 +0800

    changed socials button styling

--- client/src/lib/components/socials.svelte
+++ client/src/lib/components/socials.svelte
@@ -1,17 +1,34 @@
-<div style="padding:0 32pt;display:flex; gap:12pt">
-    <a
-        style="font-size:18pt; padding:0;"
-        href="https://www.behance.net/henrychen3de31"
+<div class="socials">
+    <a class="socials-link" href="https://www.behance.net/henrychen3de31"
         ><i class="ri-behance-fill" />
     </a>
-    <a
-        style="font-size:18pt; padding:0;"
-        href="https://www.linkedin.com/in/chenry2000/"
+    <a class="socials-link" href="https://www.linkedin.com/in/chenry2000/"
         ><i class="ri-linkedin-box-fill" /></a
     >
-    <a
-        style="font-size:18pt; padding:0;"
-        href="https://www.instagram.com/chenryhen384/"
+    <a class="socials-link" href="https://www.instagram.com/chenryhen384/"
         ><i class="ri-instagram-line" />
     </a>
 </div>
+
+<style lang="scss">
+    .socials {
+        display: flex;
+        justify-content: space-around;
+        gap: 6pt;
+        max-width: 300px;
+        margin: 24pt auto;
+
+        .socials-link {
+            border-radius: 12pt;
+            font-size: 12pt;
+            text-decoration: none;
+            color: black;
+            height: 32pt;
+            width: 32pt;
+            background: rgba(255, 255, 255, 0.8);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+</style>
