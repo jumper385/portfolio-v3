31d8a1d88760cffb904dc0dd0ca9baac3b5b2cac
Henry Chen <henrychen385@gmail.com>
2023-06-06 10:06:25 +0800

    added normalize.css resets

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -6,6 +6,10 @@
 
 <svelte:head>
     <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        rel="stylesheet"
+    />
+    <link
         href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css"
         rel="stylesheet"
     />
