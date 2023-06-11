1f1d1801ff6e636f0016e7feb14acaeeb6480d5a
Henry Chen <henrychen385@gmail.com>
2023-06-11 13:37:15 +0800

    more informative to carousel

--- client/src/lib/components/img-carousel.svelte
+++ client/src/lib/components/img-carousel.svelte
@@ -1,6 +1,10 @@
 <script>
     export let imageUrl = ["https://via.placeholder.com/150"];
     export let title;
+    export let asHeight = 2;
+    export let asWidth = 3;
+    export let borderRadius = "6pt";
+    let aspectRatio = `${asWidth} / ${asHeight}`;
     let imgIdx = 0;
 
     const nextPhoto = () => {
@@ -21,33 +25,49 @@
     };
 </script>
 
-<div class="img-carousel">
+<div class="img-carousel" style="aspect-ratio: {aspectRatio}">
     <div class="carousel-control">
         {#if imageUrl.length > 1}
-            <button class="img-carousel-nav" on:click={prevPhoto}>
+            <button
+                class="img-carousel-nav"
+                on:click={prevPhoto}
+                disabled={imgIdx == 0}
+            >
                 <i class="ri-arrow-left-s-line" />
             </button>
-            <button class="img-carousel-nav">
-                <i class="ri-arrow-right-s-line" on:click={nextPhoto} />
+            <button
+                class="img-carousel-nav"
+                disabled={imageUrl.length - 1 == imgIdx}
+                on:click={nextPhoto}
+            >
+                <i class="ri-arrow-right-s-line" />
             </button>
         {/if}
     </div>
-    <img class="card-image" src={imageUrl[imgIdx]} alt={title} />
+    <img
+        class="card-image"
+        src={imageUrl[imgIdx]}
+        alt={title}
+        style="border-radius: {borderRadius}"
+    />
+    {#if imageUrl.length > 1}
+        <div class="carousel-info">
+            <p>{imgIdx + 1} / {imageUrl.length}</p>
+        </div>
+    {/if}
 </div>
 
 <style lang="scss">
     .img-carousel {
         position: relative;
-        aspect-ratio: 3 / 2;
         img {
             position: absolute;
+            height: 100%;
             object-fit: cover;
             width: 100%;
-            aspect-ratio: 3 / 2;
             top: 0;
             left: 0;
             z-index: -100;
-            border-radius: 6pt;
         }
         .carousel-control {
             height: 100%;
@@ -72,6 +92,22 @@
                 display: flex;
                 align-items: center;
                 justify-content: center;
+            }
+        }
+        .img-carousel-nav:disabled {
+            background: rgba(255, 255, 255, 0.2);
+        }
+        .carousel-info {
+            position: absolute;
+            bottom: 0;
+            display: flex;
+            justify-content: center;
+            // background: pink;
+            padding: 0;
+            margin: 0;
+            width: 100%;
+            * {
+                margin: 0;
             }
         }
     }
