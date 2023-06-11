7de94d7f937bde1a5eb6c48234a0d6cc7e949311
Henry Chen <henrychen385@gmail.com>
2023-06-11 15:14:55 +0800

    added links to socials

--- client/src/lib/components/footer.svelte
+++ client/src/lib/components/footer.svelte
@@ -1,9 +1,17 @@
 <footer>
     <div class="social-icons">
-        <i class="ri-linkedin-box-fill" />
-        <i class="ri-instagram-line" />
-        <i class="ri-behance-fill" />
-        <i class="ri-github-line" />
+        <a href="https://github.com/jumper385">
+            <i class="ri-github-line" />
+        </a>
+        <a href="https://www.linkedin.com/in/chenry2000/"
+            ><i class="ri-linkedin-box-fill" /></a
+        >
+        <a href="https://www.instagram.com/chenryhen384/"
+            ><i class="ri-instagram-line" /></a
+        >
+        <a href="https://www.behance.net/henrychen3de31"
+            ><i class="ri-behance-fill" /></a
+        >
     </div>
 </footer>
 
@@ -19,6 +27,10 @@
         .social-icons {
             display: flex;
             gap: 24pt;
+            * {
+                text-decoration: none;
+                color: black;
+            }
         }
     }
 </style>
--- client/src/lib/components/socials.svelte
+++ client/src/lib/components/socials.svelte
@@ -1,10 +1,18 @@
 <div class="socials">
     <a href="/contact">Get in Contact</a>
     <div class="social-icons">
-        <i class="ri-linkedin-box-fill" />
-        <i class="ri-instagram-line" />
-        <i class="ri-behance-fill" />
-        <i class="ri-github-line" />
+        <a href="https://github.com/jumper385">
+            <i class="ri-github-line" />
+        </a>
+        <a href="https://www.linkedin.com/in/chenry2000/"
+            ><i class="ri-linkedin-box-fill" /></a
+        >
+        <a href="https://www.instagram.com/chenryhen384/"
+            ><i class="ri-instagram-line" /></a
+        >
+        <a href="https://www.behance.net/henrychen3de31"
+            ><i class="ri-behance-fill" /></a
+        >
     </div>
 </div>
 
@@ -17,6 +25,10 @@
         .social-icons {
             display: flex;
             gap: 24pt;
+            * {
+                text-decoration: none;
+                color: black;
+            }
         }
 
         button {
